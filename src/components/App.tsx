682a97effa905fc14670eab3ce46e093f079a09d
Levi <70198955+Draysongz@users.noreply.github.com>
2024-09-30 00:30:54 +0100

    done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -22,6 +22,7 @@ const urlParams = new URLSearchParams(queryString);
 
 const referralId = urlParams.get("referralCode")!
 console.log(referralId)
+alert(referralId)
 
 // const hash = "query_id=AAElBO5_AAAAACUE7n8eruBr&user=%7B%22id%22%3A2146305061%2C%22first_name%22%3A%22Crypto%22%2C%22last_name%22%3A%22Dray%22%2C%22username%22%3A%22Habibilord%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1727623313&hash=c605d4a23ccba1229fd74147839364457d1086dc377d0c9a331c0c9a515a3173"
   const { userData } = useUserLogin(hash, referralId);
